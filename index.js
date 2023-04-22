93e063ce011b4c2b8efc9d4e7eeb30a77834ea8e
alexavierdev <alexavier859@gmail.com>
2023-04-21 23:06:40 -0300

    fixed blur bug on body and broken layout for x-axis

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+
+
 function myFunction() {
     const nav = document.getElementById("nav");
-    const body = document.querySelector("body");
     const main = document.querySelector("main")
     const btnMenu = document.getElementById("btnMenu")
+    const body = document.querySelector("body");
 
     if(nav.className === "nav-links"){
         nav.className += " responsive";
         main.style.filter = "blur(4px)";
         btnMenu.className += " active"
+        body.style.overflow = "hidden"
     }else{
         nav.className = "nav-links";
         btnMenu.className = "fa fa-bars"
         main.style.filter = "blur(0px)";
+        body.style.overflow = "visible"
     }
 }
 
 function removeBlur(){
-    const main = document.querySelector("main")
-    main.style.filter = "blur(0px)";
-    myFunction()
+    const main = document.querySelector("main");
+    const nav = document.getElementById("nav");
+    const body = document.querySelector("body");
+
+    if(nav.className === "nav-links responsive"){
+        main.style.filter = "blur(0px)"
+        nav.className = "nav-links"
+        btnMenu.className += "icon"
+        body.style.overflow = "visible"
+    }
 }
 
